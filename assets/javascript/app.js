e78da1c20fb73c0f122475be296b3b5e7db30db6
Derek P. Cutler <43916483+DerekCutler@users.noreply.github.com>
2018-11-23 16:27:35 -0700

    start with

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// Start up 
+let finalCountDown = 42;
+
+
+
+
+
+
+
+    // Show in the HTMLs
+    $("#theTimerGoesHere").html("<h1>How much time do I have left: " + "0:42" + "</h2>");
+    $("#score").html("<h2>Score: " + score + "</h2>");
+
+    $("#winCount").html("<h2>Wins: " + winCount + "</h2>");
+    $("#loseCount").html("<h2>Losses: " + loseCount + "</h2>");
+
+
+
+
+
+
+// ** FIN **
