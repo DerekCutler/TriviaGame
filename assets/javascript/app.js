c38821fa18ed134de14ad0bac7cc144751212b3e
Derek P. Cutler <43916483+DerekCutler@users.noreply.github.com>
2018-11-25 10:36:09 -0700

    general format complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,38 @@
-// Start up 
-let finalCountDown = 42;
+// Start With Variables
+
+var sec = 21
 
 
 
 
+// Functions
 
+var timer = setInterval(function() { 
+    $('#time').text(sec--);
+    if (sec === 0) {
+       $('#time').fadeOut('fast');
+       alert("It looks like you ran out of time!")
+     //   clearInterval(timer);
+    } 
+ }, 1000);
+
+
+
+// On Click Events
 
 
     // Show in the HTMLs
-    $("#theTimerGoesHere").html("<h1>How much time do I have left: " + "0:42" + "</h2>");
-    $("#score").html("<h2>Score: " + score + "</h2>");
+    $("#time").html("<h1>How much time do I have left: " + timer + "</h2>");
 
-    $("#winCount").html("<h2>Wins: " + winCount + "</h2>");
-    $("#loseCount").html("<h2>Losses: " + loseCount + "</h2>");
+$("input[type="radio"]:checked")
 
+
+
+    // <!-- Default unchecked --> Radio HTM Example
+    // <div class="radio">
+    //     <input type="radio" class="custom-control-input" id="defaultUnchecked" name="defaultExampleRadios">
+    //     <label class="custom-control-label" for="defaultUnchecked">Default unchecked</label>
+    // </div>
 
 
 
